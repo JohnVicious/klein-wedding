3b3d4e2b0afcfd710e785faa053de5ffcbda2022
JohnKlein985 <John@apexinnovations.com>
2023-07-27 20:57:22 -0500

    Base shell

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { onMounted, onUnmounted } from "vue";
+
+//example components
+
+//hooks
+onMounted(() => {
+});
+onUnmounted(() => {
+});
+</script>
+
+<template>
+  <div class="container position-sticky z-index-sticky top-0">
+    <div class="row">
+      <div class="col-12">
+        HIYA - Im getting married!
+      </div>
+    </div>
+  </div>
+   
+</template>
