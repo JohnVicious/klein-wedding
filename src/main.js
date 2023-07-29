1a20148fa22dc43c08ec85d5432747a44449299b
JohnKlein985 <John@apexinnovations.com>
2023-07-28 21:20:51 -0500

    Adding starting touches

--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ import App from "./App.vue";
 import router from "./router";
 
 // Nucleo Icons
-import "./assets/css/nucleo-icons.css";
-import "./assets/css/nucleo-svg.css";
+// import "./assets/css/nucleo-icons.css";
+// import "./assets/css/nucleo-svg.css";
 
 import materialKit from "./material-kit";
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,19 +2,63 @@
 import { onMounted, onUnmounted } from "vue";
 
 //example components
+function days_between(date1, date2) {
+
+  // The number of milliseconds in one day
+  const ONE_DAY = 1000 * 60 * 60 * 24;
+
+  // Calculate the difference in milliseconds
+  const differenceMs = Math.abs(date1 - date2);
+
+  // Convert back to days and return
+  return Math.round(differenceMs / ONE_DAY);
+
+}
 
 //hooks
 onMounted(() => {
+  
+  const daysLeftCount = days_between(
+      new Date(),
+      new Date("2023-11-03")
+    );
+    const divElement = document.getElementById('daysLeftCount');
+    divElement.textContent = daysLeftCount;
 });
 onUnmounted(() => {
 });
 </script>
 
 <template>
+  
   <div class="container position-sticky z-index-sticky top-0">
-    <div class="row">
-      <div class="col-12">
-        HIYA - Im getting married!
+    <div class="row mt-6 inviteCard border-0 shadow p-4" style="min-height:500px;">
+      <div class="col-12 text-center">
+
+
+        <div class="h-50 d-inline-block ">
+
+          <!-- Header -->
+          <div class="row">
+            <div class="col-12 text-center nameSignature">
+              John & Jessica
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-12 text-center subHeadText">
+              November 3rd, 2023 | Ridgedale, MO
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-12 text-center subHeadText">
+              <span id="daysLeftCount"></span> Days To Go
+            </div>
+          </div>
+
+
+
+        </div>
+
       </div>
     </div>
   </div>
