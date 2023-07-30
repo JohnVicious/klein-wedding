ea5a4cf47340540bf483e1308a2e2122227b113e
JohnKlein985 <John@apexinnovations.com>
2023-07-30 00:20:50 -0500

    Added itinerary rough draft

--- src/views/Itinerary.vue
+++ src/views/Itinerary.vue
@@ -16,17 +16,77 @@ onUnmounted(() => {
 <template>
   
   <div class="container position-sticky z-index-sticky top-0">
-    <div class="row mt-6 inviteCard border-0 shadow p-4" >
+    <div class="row mt-6 mb-6 inviteCard border-0 shadow p-4" >
       <div class="col-12 text-center">
         <div class="h-50 d-inline-block ">
           <Banner/>
           <Navlinks/>
 
-          <div class="row mt-4">
+          <!-- THURSDAY -->
+          <div class="row mt-5">
             <div class="col-12">
-              TBA
+              <div class="itinerary-dayheader allura text-center">
+                <img src="@/assets/img/dayheader_leftmark.svg" />
+                <span>Thursday</span>
+                <img src="@/assets/img/dayheader_rightmark.svg" />
+              </div>
             </div>
           </div>
+          <div class="row mt-2 itinerary-eventpanel">
+            <div class="col-12">
+              <div class="row">
+                <div class="col-6 itinerary-eventtitle py-2">
+                  <div>GOLF CART TOUR</div>
+                </div>
+                <div class="col-6 itinerary-eventtime py-2">
+                  2:00 P.M.
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-6 itinerary-eventtitle py-2">
+                  <div>REHERSAL DINNER</div>
+                  <div class="font-italic">MOUNTAIN TOP</div>
+                </div>
+                <div class="col-6 itinerary-eventtime py-2">
+                  5:00 P.M.
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <!-- FRIDAY -->
+          <div class="row mt-5">
+            <div class="col-12">
+              <div class="itinerary-dayheader allura text-center">
+                <img src="@/assets/img/dayheader_leftmark.svg" />
+                <span>Friday</span>
+                <img src="@/assets/img/dayheader_rightmark.svg" />
+              </div>
+            </div>
+          </div>
+          <div class="row mt-2 itinerary-eventpanel">
+            <div class="col-12">
+              <div class="row">
+                <div class="col-6 itinerary-eventtitle py-2">
+                  <div>WEDDING</div>
+                </div>
+                <div class="col-6 itinerary-eventtime py-2">
+                  2:00 P.M.
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-6 itinerary-eventtitle py-2">
+                  <div>DINNER</div>
+                  <div class="font-italic">TOP OF THE ROCK</div>
+                </div>
+                <div class="col-6 itinerary-eventtime py-2">
+                  5:00 P.M.
+                </div>
+              </div>
+            </div>
+          </div>
+
+
 
         </div>
       </div>
